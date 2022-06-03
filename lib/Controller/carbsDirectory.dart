bf5b118ab6e59034e3c4e06be78af60e4fbb4693
shishir <ccrsharma7@gmail.com>
2022-06-03 09:54:43 +0200

    merged shishir branch and made changes in the carbsdirectory

--- lib/Controller/carbsDirectory.dart
+++ lib/Controller/carbsDirectory.dart
@@ -2,28 +2,52 @@
 
 getCarbs() {
   return {
+    // A
     "almond": 0.184,
     "apple": 0.143,
     "asparagus": 0.049,
     "artichoke": 0.105,
+
+    // B
     "banana": 0.226,
     "broccoli": 0.063,
     "butter": 0.012,
+
+    // C
     "cabbage": 0.073,
     "carrot": 0.089,
     "cauliflower": 0.047,
     "chocolate": 0.581,
     "cream": 0.02,
+
+    // E
     "egg": 0.015,
+
+    // F
     "flour": 0.738,
+
+    // L
     "lettuce": 0.035,
+
+    // M
     "milk": 0.046,
+
+    // O
     "orange": 0.122,
+    "oil": 0,
+
+    // P
     "potato": 0.173,
+
+    // R
     "raspberry": 0.106,
+
+    // S
     "spinach": 0.032,
     "strawberry": 0.083,
     "salt": 0,
+
+    // W
     "waffle": 0.1905,
     "water": 0,
   };
