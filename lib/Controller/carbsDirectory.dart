f8d2bab5ecf0248fe4e014a141186406f179b96a
shishir <ccrsharma7@gmail.com>
2022-06-14 13:10:11 +0200

    small changes in the carbs directory

--- lib/Controller/carbsDirectory.dart
+++ lib/Controller/carbsDirectory.dart
@@ -2,59 +2,48 @@
 
 getCarbs() {
   return {
-    // A
-    "almond": 0.184,
+    "almond": 0.184, // A
     "apple": 0.143,
     "asparagus": 0.049,
     "artichoke": 0.105,
     "avocado": 0.0,
 
-    // B
-    "banana": 0.226,
+    "banana": 0.226, // B
     "broccoli": 0.063,
     "butter": 0.012,
-    "bread":0.0,
+    "bread": 0.45,
 
-    // C
-    "cabbage": 0.073,
+    "cabbage": 0.073, // C
     "carrot": 0.089,
+    "cashew": 0.327,
     "cauliflower": 0.047,
     "chocolate": 0.581,
     "cream": 0.02,
-    "courgette":0.16,
+    "courgette": 0.16,
 
-    // E
-    "egg": 0.015,
+    "egg": 0.015, // E
 
-    // F
-    "flour": 0.738,
+    "flour": 0.738, // F
 
-    // L
-    "lettuce": 0.035,
-    "leek": 0.0,
+    "lettuce": 0.035, // L
+    "leek": 0.078,
 
-    // M
-    "milk": 0.046,
+    "milk": 0.046, // M
 
-    // O
-    "orange": 0.122,
+    "orange": 0.122, // O
     "oil": 0.0,
 
-    // P
-    "potato": 0.173,
+    "potato": 0.173, // P
     "pepper": 0.725,
 
-    // R
-    "raspberry": 0.106,
+    "raspberry": 0.106, // R
 
-    // S
-    "spinach": 0.032,
+    "spinach": 0.032, // S
     "strawberry": 0.083,
     "salt": 0,
     "sugar": 0.999,
 
-    // W
-    "waffle": 0.1905,
+    "waffle": 0.1905, // W
     "water": 0,
   };
 }
